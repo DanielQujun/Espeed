5b300a2e278da2f71f107cc39b9f057f3538cfd6
wenzehai <373952776@qq.com>
2018-05-15 23:59:43 +0800

    修改worklist获取openid方法

---
+++ Espeed/static/js/utility.js
@@ -0,0 +1,48 @@
+
+function lacation() {
+    wx.getLocation({
+        type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+        success: function (res) {
+            var $latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+            var $longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+            var $speed = res.speed; // 速度，以米/每秒计
+            var $accuracy = res.accuracy; // 位置精度
+
+
+            //转换为街道地址，写入页面中
+            getBaiduPosition($longitude,$latitude)
+
+        }
+    });
+}
+
+//调用百度api转化坐标系
+function getBaiduPosition(lng,lat) {
+    var url ="http://api.map.baidu.com/geoconv/v1/?coords="+lng+","+lat+"&from=1&to=5&ak=BVbS3AVAs8iNor6NGxKfc1gG5OcqTxBu";
+    $.ajax({
+        url: url,
+        type: 'GET',
+        contentType: "application/json",
+        dataType: 'jsonp',//这里要用jsonp的方式不然会报错
+        success: function(data){
+
+            //将转化完的坐标，写入session中，方便ajax发送已经转化过了的地址信息；
+            $.session.set('latitude', data.result[0].y);
+            $.session.set('longitude', data.result[0].x);
+
+            // 根据坐标得到地址描述
+            myGeo.getLocation(new BMap.Point($.session.get("longitude"),$.session.get("latitude")), function (result) {
+                if (result) {
+                    //地址写入页面
+                    $("#location").text(result.address);
+                } else {
+                    $.alert("获取位置失败...","text");
+                }
+            });
+
+        },
+        error: function(obj) {
+            alert(obj.status);
+        }
+    });
+}
