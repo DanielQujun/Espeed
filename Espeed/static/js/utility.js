4b2dee8482c544da76da95a4c85c5f7e506e76ba
wenzehai <373952776@qq.com>
2018-11-14 22:17:23 +0800

    add alert

--- Espeed/static/js/utility.js
+++ Espeed/static/js/utility.js
@@ -1,5 +1,6 @@
 
 function lacation() {
+    alert("开始获取地址")
     wx.getLocation({
         type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
         success: function (res) {
@@ -8,6 +9,7 @@ function lacation() {
             var $speed = res.speed; // 速度，以米/每秒计
             var $accuracy = res.accuracy; // 位置精度
 
+            alert("获取成功")
 
             //转换为街道地址，写入页面中
             getBaiduPosition($longitude,$latitude)
@@ -18,6 +20,7 @@ function lacation() {
 
 //调用百度api转化坐标系
 function getBaiduPosition(lng,lat) {
+    alert("开始转化坐标")
     var url ="http://api.map.baidu.com/geoconv/v1/?coords="+lng+","+lat+"&from=1&to=5&ak=BVbS3AVAs8iNor6NGxKfc1gG5OcqTxBu";
     $.ajax({
         url: url,
@@ -35,6 +38,7 @@ function getBaiduPosition(lng,lat) {
             myGeo.getLocation(new BMap.Point($.session.get("longitude"),$.session.get("latitude")), function (result) {
                 if (result) {
                     //地址写入页面
+                    alert("转化成功");
                     $("#location").text(result.address);
                 } else {
                     $.alert("获取位置失败...","text");
