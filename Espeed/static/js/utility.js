d3adb4dbb3eef19b4c6902ab463b0f0447860bd5
wenzehai <373952776@qq.com>
2018-11-14 22:29:58 +0800

    remove alert

--- Espeed/static/js/utility.js
+++ Espeed/static/js/utility.js
@@ -1,6 +1,6 @@
 
 function lacation() {
-    alert("开始获取地址")
+    // alert("开始获取地址")
     wx.getLocation({
         type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
         success: function (res) {
@@ -9,7 +9,7 @@ function lacation() {
             var $speed = res.speed; // 速度，以米/每秒计
             var $accuracy = res.accuracy; // 位置精度
 
-            alert("获取成功")
+            // alert("获取成功")
 
             //转换为街道地址，写入页面中
             getBaiduPosition($longitude,$latitude)
@@ -20,7 +20,7 @@ function lacation() {
 
 //调用百度api转化坐标系
 function getBaiduPosition(lng,lat) {
-    alert("开始转化坐标")
+    // alert("开始转化坐标")
     var url ="http://api.map.baidu.com/geoconv/v1/?coords="+lng+","+lat+"&from=1&to=5&ak=BVbS3AVAs8iNor6NGxKfc1gG5OcqTxBu";
     $.ajax({
         url: url,
@@ -32,8 +32,8 @@ function getBaiduPosition(lng,lat) {
             //将转化完的坐标，写入session中，方便ajax发送已经转化过了的地址信息；
             $.session.set('latitude', data.result[0].y);
             $.session.set('longitude', data.result[0].x);
-            alert( $.session.getItem('latitude'));
-            alert( $.session.getItem('longitude'));
+            // alert( $.session.getItem('latitude'));
+            // alert( $.session.getItem('longitude'));
             // 根据坐标得到地址描述
             myGeo.getLocation(new BMap.Point($.session.get("longitude"),$.session.get("latitude")), function (result) {
                 if (result) {
